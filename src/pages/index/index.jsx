c869d63f18c550641ee7d15d6b5c3dbe81fab11d
ltslyc <lts.lyc@qq.com>
2020-12-24 18:57:14 +0800

    初始化小程序模板

---
+++ src/pages/index/index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { View, Text } from '@tarojs/components'
+import { AtButton } from 'taro-ui'
+
+import "taro-ui/dist/style/components/button.scss" // 按需引入
+import './index.less'
+
+const Index = () => {
+  return (
+    <View className='index'>
+      
+    </View>
+  )
+}
+
+export default Index
